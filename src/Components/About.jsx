2602b1059a1f45c93702c014876bf46d73f551fa
GowthamAbi <gowtham2131ece@gmail.com>
2025-04-03 17:54:15 +0530

    About

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,7 @@ function About() {
         <div className="relative">
             <div className="flex">
                 {/* Left Panel */}
-                <div className="bg-[#fae5d3] w-1/4 h-[150px]"></div>
+                <div className="bg-[#fae5d3] w-1/4 h-150"></div>
 
                 {/* Right Panel */}
                 <div className="bg-white w-full m-10 ml-10 p-5">
@@ -15,9 +15,7 @@ function About() {
                                            transition duration-300">
                             RESUME
                         </button>
-                        <button className="text-black border px-8 py-2 rounded-3xl 
-                                           hover:bg-green-500 hover:text-white hover:border-green-500 
-                                           transition duration-300">
+                        <button className="text-black border px-8 py-2 rounded-3xl hover:bg-green-700">
                             PROJECTS
                         </button>
                     </div>
@@ -33,24 +31,18 @@ function About() {
 
             {/* Profile Card */}
             <div className="absolute top-10 left-40 lg:left-20 bg-[#fae3d1] w-[200px] h-[250px] 
-                            shadow-lg rounded-lg text-center p-4">
+                            shadow-lg rounded-lg text-center ">
                 <img src="/profile/Profile.jpg" alt="Gowtham's Profile" 
                      className="w-24 h-24 mx-auto rounded-full" />
                 <h1 className="text-xl font-bold mt-2">Gowtham</h1>
                 <h2 className="text-gray-700">Software Developer</h2>
-                
-                {/* Social Media Icons */}
-                <div className="flex justify-center gap-3 mt-6 bg-white p-2 w-full rounded-lg">
-                    <img src="/icons/gmail.png" alt="Mail" className="w-6 h-6 cursor-pointer hover:scale-110 transition-transform" />
-                    <img src="/icons/linkedin.png" alt="LinkedIn" className="w-6 h-6 cursor-pointer hover:scale-110 transition-transform" />
-                    <img src="/icons/social.png" alt="Social" className="w-6 h-6 cursor-pointer hover:scale-110 transition-transform" />
-                    <img src="/icons/twitter.png" alt="Twitter" className="w-6 h-6 cursor-pointer hover:scale-110 transition-transform" />
+                <div className="flex justify-center gap-3 mt-6 bg-white p-2 mt-14 w-[200px]">
+                    <img src="/icons/gmail.png" alt="Mail" className="w-6 h-6" />
+                    <img src="/icons/linkedin.png" alt="LinkedIn" className="w-6 h-6" />
+                    <img src="/icons/social.png" alt="Social" className="w-6 h-6" />
+                    <img src="/icons/twitter.png" alt="Twitter" className="w-6 h-6" />
                 </div>
             </div>
-
-            {/* Additional Sections */}
-            <div className="bg-[#fae5d3] w-2/4 h-[180px]"></div>
-            <div className="bg-white w-full h-[160px]"></div>
         </div>
     );
 }
